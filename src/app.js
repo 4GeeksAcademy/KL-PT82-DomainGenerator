89fd0192d26b7cbaf1556f1b4e20652c4dc0f534
K1ddSuavee <kelvinlawler96@icloud.com>
2025-06-10 02:16:26 +0000

    Finished product

--- src/app.js
+++ src/app.js
@@ -1,39 +1,36 @@
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function () {
-  //write your code here
   let pronoun = ["the", "our", "his", "her", "their"];
   let adj = ["great", "big", "blue", "simple", "amazing"];
   let noun = ["jogger", "racoon", "sky", "city", "home"];
+  let extensions = [".com", ".net", ".org", ".io"]; // Add more extensions here
 
   function randomDomainName() {
     let randomPronoun = pronoun[Math.floor(Math.random() * pronoun.length)];
     let randomAdj = adj[Math.floor(Math.random() * adj.length)];
     let randomNoun = noun[Math.floor(Math.random() * noun.length)];
+    let randomExt = extensions[Math.floor(Math.random() * extensions.length)];
 
-    return `${randomPronoun}${randomAdj}${randomNoun}.com`;
+    return `${randomPronoun}${randomAdj}${randomNoun}${randomExt}`;
   }
+
   let result = randomDomainName();
   document.querySelector("#domain").innerText = result;
-  console.log("Hello Rigo from the console!");
+
   function getAllDomains() {
     let AllDomains = [];
     for (let i = 0; i < pronoun.length; i++) {
       for (let j = 0; j < adj.length; j++) {
         for (let k = 0; k < noun.length; k++) {
-          let domain = `${pronoun[i]}${adj[j]}${noun[k]}.com`;
-          AllDomains.push(domain);
+          for (let l = 0; l < extensions.length; l++) {
+            let domain = `${pronoun[i]}${adj[j]}${noun[k]}${extensions[l]}`;
+            AllDomains.push(domain);
+          }
         }
       }
     }
     return AllDomains;
   }
-  let domainsResults = getAllDomains();
-  document.querySelector("#allDomains").innerText = domainsResults.join("<br>");
 
-  console.log(result);
+  let domainsResults = getAllDomains();
+  document.querySelector("#allDomains").innerHTML = domainsResults.join("<br>");
 };
